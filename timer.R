50b3e296822b2f890077d5d70ed104dc8ba05009
Thomas Lister <thomas.lister@hotmail.no>
2023-10-30 23:04:57 +0100

    Finishing the assignment

--- timer.R
+++ timer.R
@@ -1,20 +1,20 @@
-# Load the tictoc package
+
 library(tictoc)
 
-# List of script files to source from the scripts\ directory
+#Storing the script files
 script_files <- list.files("scripts", full.names = TRUE)
 
 # Measure the execution time for each script
 for (script_file in script_files) {
   cat("Executing script:", script_file, "\n")
   
-  # Start the timer
+
   tic()
   
-  # Source the script file
+ 
   source(script_file)
   
-  # Stop the timer and print the elapsed time
+
   toc()
   
   cat("\n")
